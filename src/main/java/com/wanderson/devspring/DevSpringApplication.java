8d36e328044ae659a2a9cfc95ef310d1c6de86c0
Wanderson Alves Santos <wandersonalves.s@hotmail.com>
2022-11-04 08:38:33 -0300

    first commit

---
+++ src/main/java/com/wanderson/devspring/DevSpringApplication.java
@@ -0,0 +1,13 @@
+package com.wanderson.devspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wanderson/devspring/DevSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanderson.devspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
